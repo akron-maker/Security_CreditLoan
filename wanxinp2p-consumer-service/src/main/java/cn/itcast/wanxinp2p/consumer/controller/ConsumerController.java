a74475a75e9c018c2b7a35c8422093ea22d83abf
akron <1511553138@qq.com>
2022-04-04 22:54:41 +0800

    删除测试controller

--- wanxinp2p-consumer-service/src/main/java/cn/itcast/wanxinp2p/consumer/controller/ConsumerController.java
+++ wanxinp2p-consumer-service/src/main/java/cn/itcast/wanxinp2p/consumer/controller/ConsumerController.java
@@ -34,18 +34,6 @@ public class ConsumerController implements ConsumerAPI {
         @Autowired
         private ConsumerService consumerService;
 
-        @ApiOperation("测试hello")
-        @GetMapping(path = "/hello")
-        public String hello(){
-            return "hello";
-        }
-
-        @ApiOperation("测试hi")
-        @PostMapping(path="/hi")
-        @ApiImplicitParam(name="name",value = "姓名",required = true,dataType = "String")
-        public String hi(String name){
-            return "hi,"+name;
-        }
 
     @Override
     @ApiOperation("用户注册")
